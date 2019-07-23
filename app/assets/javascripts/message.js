5790364462c40dae394c18073ef77b2b454547ef
keisuke <keisuke.002.tus@gmail.com>
2019-07-23 22:14:23 +0900

    updata

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,14 @@
 $(function(){
-    $('#new_message').on('submit', function(e){
+    function buildHTML(message){
+        var html = `<p class="talkspace__middle__username"> ${message.name}</p>
+                    <p class="talkspace__middle__time">${message.created_at}</p>
+                    <p class="talkspace__middle__message">${message.body}</p>`
+        return html;
+    };
+    $("#new_message").on('submit', function(e){
       e.preventDefault();
       var formData = new FormData(this);
+      $('#submit-btn').removeAttr('data-disable-with')
       var url = $(this).attr('action')
       $.ajax({
           url:  url,
@@ -11,5 +18,16 @@ $(function(){
           processData: false,
           contentType: false
       })
-    })
-  })
+      .done(function(data){
+          var html = buildHTML(data);
+          $('.talkspace__middle').append(html);
+          $('.talkspace__middle__message').val('');
+          $this.get(0).reset();
+          autoScroll();
+      })
+      .fail(function(){
+          alert('error');
+      });
+      return false;
+    });
+  });
