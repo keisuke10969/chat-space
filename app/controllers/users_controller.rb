05dbaa9c58ca5fb4501396d51857cd8a9082828e
keisuke <keisuke.002.tus@gmail.com>
2019-07-26 16:46:48 +0900

    update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
     def index
-        @users = User.where('name LIKE(?)', "%#{params[:keyword]}%")
+        @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").where.not(id: current_user.id)
         respond_to do |format|
             format.json
+            format.html
         end   
       end
     
