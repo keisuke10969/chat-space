98ffbd990249fc7f181b05f5eb9aaf73baaa182b
keisuke <keisuke.002.tus@gmail.com>
2019-07-27 18:11:49 +0900

    update

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,12 +1,6 @@
 class Api::MessagesController < ApplicationController
     def index
-        @message = Message.new
-        @messages = @group.messages.includes(:user)
-        @members = @group.users
-        respond_to do |format|
-            format.html
-            format.json { @messages = @messages.where("id > ?", params[:last_id]) }
-            binding.pry
-        end
+        @group = Group.find(params[:group_id])
+        @messages = @group.messages.includes(:user).where('id > ?', params[:id])
     end
   end
